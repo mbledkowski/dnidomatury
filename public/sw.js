29a80a98129ca6e2c45c9584ab01406e95b913d3
Maciej Błędkowski <23295125+mmble@users.noreply.github.com>
2020-05-07 15:57:08 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', event => {
   event.waitUntil( //"event.waitUntil takes a promise to define the length & success of the install. If the promise rejects, the installation is considered a failure and this ServiceWorker will be abandoned" Source: developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook
     caches.open('dnidomatury').then(cache => {
-      cache.addAll([ //"We're not passing the cache.addAll promise for levels 11-20 back to event.waitUntil, so even if it fails, the game will still be available offline." Same source as above.
+      cache.addAll([ //"We're not passing the cache.addAll promise [...] back to event.waitUntil, so even if it fails, the game will still be available offline." Same source as above.
         '/miesiące',
         '/godziny',
         '/minuty',
@@ -34,18 +34,6 @@ self.addEventListener('install', event => {
     })
   )
 })
-/*
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(cacheNames.filter(cacheName => {
-        return true
-      })).map(cacheName => {
-        return caches.delete(cacheName)
-      })
-    })
-  )
-})*/
 
 self.addEventListener('fetch', event => {
   if (!navigator.onLine) {
