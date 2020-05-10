db9f8e507c6fbadacc28037805f935340aa3fe46
Maciej Błędkowski <23295125+mmble@users.noreply.github.com>
2020-05-10 18:09:14 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -50,7 +50,7 @@ self.addEventListener('fetch', event => {
     event.respondWith(
       fetch(event.request).catch(() => {
         return caches.match(event.request)
-      })
-    )
+      });
+    );
   }
 })
