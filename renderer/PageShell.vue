e6ecfd161e12e4fd09671f1f2a309afa308de316
Maciej Błędkowski <23295125+mbledkowski@users.noreply.github.com>
2022-06-29 02:46:51 +0200

    Remove unnecessary file

--- renderer/PageShell.vue
+++ renderer/PageShell.vue
@@ -2,12 +2,13 @@
   <div class="layout">
     <div class="navigation">
       <a href="/" class="logo">
-        <img src="./logo.svg" height="64" width="64" alt="logo" />
       </a>
       <Link href="/">Home</Link>
       <Link href="/about">About</Link>
     </div>
-    <div class="content"><slot /></div>
+    <div class="content">
+      <slot />
+    </div>
   </div>
 </template>
 
@@ -20,9 +21,11 @@ body {
   margin: 0;
   font-family: sans-serif;
 }
+
 * {
   box-sizing: border-box;
 }
+
 a {
   text-decoration: none;
 }
@@ -34,12 +37,14 @@ a {
   max-width: 900px;
   margin: auto;
 }
+
 .content {
   padding: 20px;
   border-left: 2px solid #eee;
   padding-bottom: 50px;
   min-height: 100vh;
 }
+
 .navigation {
   padding: 20px;
   flex-shrink: 0;
@@ -48,6 +53,7 @@ a {
   align-items: center;
   line-height: 1.8em;
 }
+
 .logo {
   margin-top: 20px;
   margin-bottom: 10px;
