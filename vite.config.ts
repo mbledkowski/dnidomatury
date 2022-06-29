bc9a037ffd4cd0f89a281dccc3ca523a711c798b
Maciej Błędkowski <23295125+mbledkowski@users.noreply.github.com>
2022-06-29 02:24:11 +0200

    Using vite-plugin-ssr

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import ssr from 'vite-plugin-ssr/plugin'
+import { UserConfig } from 'vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+const config: UserConfig = {
+  plugins: [vue(), ssr()],
+}
+
+export default config
