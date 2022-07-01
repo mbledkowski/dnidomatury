20a63ae677a075676b1c3af2cc42d817dc5a747f
Maciej Błędkowski <23295125+mbledkowski@users.noreply.github.com>
2022-07-01 05:56:46 +0200

    Update test and jest config

--- test/nav.spec.js
+++ test/nav.spec.js
@@ -1,9 +1,34 @@
-import { mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import Nav from '@/components/Nav.vue'
 
 describe('Nav', () => {
+  const wrapper = shallowMount(Nav, {
+    stubs: {
+      'nuxt-link': true,
+      'any-other-child': true,
+    },
+    mocks: {
+      // Always returns the input
+      $t: (i) => i,
+      localePath: (i) => i,
+    },
+  })
   it('renders correctly', () => {
-    const wrapper = mount(Nav)
+    expect(wrapper.element).toMatchSnapshot()
+  })
+  it('renders correctly with links', () => {
+    wrapper.setProps({
+      links: [
+        {
+          name: 'timer',
+          path: '/',
+        },
+        {
+          name: 'settings',
+          path: '/settings',
+        },
+      ],
+    })
     expect(wrapper.element).toMatchSnapshot()
   })
 })
