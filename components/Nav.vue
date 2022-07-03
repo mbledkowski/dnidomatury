d48f455f02802e43b63b486cef47517f244611ef
Maciej Błędkowski <23295125+mbledkowski@users.noreply.github.com>
2022-07-03 21:40:54 +0200

    Colored links in navbars

--- components/Nav.vue
+++ components/Nav.vue
@@ -36,6 +36,9 @@ nav.main-nav {
   @apply flex flex-row justify-evenly items-center mx-auto text-white bg-black rounded-none md:rounded-lg h-16 md:h-8 text-lg font-bold w-full md:w-96 lowercase fixed bottom-0 z-10 md:static md:my-8;
   > a {
     @apply w-1/4 md:w-auto text-center;
+    &.nuxt-link-active {
+      @apply text-yellow-300;
+    }
     > img {
       @apply mx-auto;
     }
